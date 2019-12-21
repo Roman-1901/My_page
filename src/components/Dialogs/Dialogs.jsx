e80007476c02d0ff98096ca5b7827673365876e0
Roman <roman-1901@mail.ru>
2019-12-21 20:20:55 +0300

    addPost1

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -7,18 +7,29 @@ import Message from './Messages/Messages';
 
 const Dialogs = (props) => {
 
-
-    
     let dialog = props.state.dialogItem.map(d => <DialogItem id = {d.id} name = {d.name} ava = {d.ava}/>);
     let message = props.state.messageItem.map(m =>  <Message messageMy = {m.messageMy} messageFriend = {m.messageFriend} />);
 
-        return <div className={classes.dialogs}>
+    let newTextElement = React.createRef();
+
+    let addText = () => {
+        let text = newTextElement.current.value;
+        alert(text);
+    }
+
+        return <div>
+                <div className={classes.dialogs}>
                     <div className={classes.dialogsItems}>
                         {dialog}
                     </div>
                     <div className={classes.messages}>
                         {message}
                     </div>
+                </div>
+                <div className={classes.addText}>
+                    <div><textarea ref = {newTextElement}></textarea></div>
+                    <div><button onClick = {addText}>Add text</button></div>  
+                </div>
                 </div>
 }
 
--- src/components/Profile/MyPosts/Posts.jsx
+++ src/components/Profile/MyPosts/Posts.jsx
@@ -7,11 +7,18 @@ const Posts= (props) => {
     
     let post = props.postData.map(p => <Post message = {p.message} count = {p.count} ava = {props.profileAvatar}/>);
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
     return  <div className={classes.posts}> 
                     <div className={classes.myPosts}>
                     <div>My posts</div>
-                    <div><textarea></textarea></div>
-                    <div><button>Add post</button></div>    
+                    <div><textarea ref = {newPostElement}></textarea></div>
+                    <div><button onClick = {addPost}>Add post</button></div>    
                     </div> 
                     {post}
             </div>
